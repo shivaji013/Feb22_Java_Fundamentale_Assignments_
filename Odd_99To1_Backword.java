15f4ebd43d1aba561536b1dadc23af9423f74007
shivaji <shivajixxx13@gmail.com>
2022-02-20 02:41:49 +0530

    WAP TO PRINT ODD NUMBER BACKWORD FROM 99 to 1

---
+++ Odd_99To1_Backword.java
@@ -0,0 +1,16 @@
+
+public class Odd_99To1_Backword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=99;i>=1;i--)
+		{
+			if(i%2==1)
+			{
+				System.out.println("Odd Number = "+i);
+			}		
+		}
+	}
+
+}
