098ed408e41e72c8b7514e7c24bf2669f9a89a09
shivaji <shivajixxx13@gmail.com>
2022-07-29 10:09:00 +0530

    first7DivBy6.java

--- first7DivBy6.java
+++ first7DivBy6.java
@@ -1,7 +1,5 @@
 package Assignment_java.Feb22_Java_Fundamentale_Assignments_;
 
-import java.util.Scanner;
-
 public class first7DivBy6 {
 
 	public static void main(String[] args)
@@ -9,14 +7,14 @@ public class first7DivBy6 {
 
 		//first 7 terms of (n*6n) where divisible by 6
 		
-		System.out.println("Enter the number ::");
+			System.out.println("First 7 num Divisible by 6    ::::::::");
+			int num=7;
+			for(int i=1;i<=num;i++)
+			{	
+				int div=num*(6*i);
+				System.out.println("Divisible by 6 :: "+div);
+			}
 		
-		int num=7;
-		for(int i=1;i<=num;i++)
-		{	
-			int div=i*(6*i);
-			System.out.println("Divisible by 6 ::"+div);
-		}
 	}
 
 }
