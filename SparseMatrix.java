6e5fa7e570d87eea56a98863ce2ea4ead92ae9fa
shivaji <shivajixxx13@gmail.com>
2022-07-29 10:07:36 +0530

    SparseMatrix

--- SparseMatrix.java
+++ SparseMatrix.java
@@ -13,14 +13,14 @@ public class SparseMatrix {
 		System.out.print("Enter the Col matrix by Any 2,3,4 :");
 		int col=sc.nextInt();
 		
-		int mat[][]=new int[row][col];
+		int mat[][] = new int[row][col];
 		int zeroMax=0;
-		for(int i=0;i<row;i++)
+		for(int i=0; i<row; i++)
 		{
-			for(int j=0;j<col;j++)
+			for(int j=0; j<col; j++)
 			{
-				mat[i][j]=sc.nextInt();
-				if(mat[i][j]==0)
+				mat[i][j] = sc.nextInt();
+				if(mat[i][j] == 0)
 				{
 					zeroMax++;
 				}	
